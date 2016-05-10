8cbe91cae75c8c6c10b21ea8359bb4b818d7c67c
Leonid Dinershtein <leonid@dinershtein.com>
2016-05-10 13:16:04 +0300

    Map

--- _site/js/grayscale.js
+++ _site/js/grayscale.js
@@ -52,7 +52,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(59.7121065, 30.4481283),
+        center: new google.maps.LatLng(59.711608,30.4458863),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -180,7 +180,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(59.7121065, 30.4481283);
+    var myLatLng = new google.maps.LatLng(59.711608, 30.4458863);
 
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
